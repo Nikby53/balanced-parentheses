b18a6e88fe29e5f723fe4780b6d2405b58f55098
nikby53 <nikdiablo3@gmail.com>
2021-08-16 18:55:39 +0300

    add comments for errors.go

--- bracketsGenerator/errors.go
+++ bracketsGenerator/errors.go
@@ -3,6 +3,8 @@ package bracketsGenerator
 import "errors"
 
 var (
-	ErrIncorrectInput  = errors.New("incorrect input")
+	// ErrIncorrectInput checks for incorrect input
+	ErrIncorrectInput = errors.New("incorrect input")
+	// ErrIncorrectNumber checks that number must be greater than zero
 	ErrIncorrectNumber = errors.New("enter a number that is greater than zero")
 )

620f457cc875b195236b50883d7102703729cbd6
nikby53 <nikdiablo3@gmail.com>
2021-08-18 18:28:14 +0300

    added some changes

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -18,19 +18,17 @@ func (s serviceMock) Generate(_ int) (string, error) {
 func TestGenerationHandler(t *testing.T) {
 	req := httptest.NewRequest("GET", "localhost:8081/generate?n=2", nil)
 	rec := httptest.NewRecorder()
-	h := New(serviceMock{
-		err: nil,
-	})
+	h := New(serviceMock{err: nil})
 	h.GenerationHandler(rec, req)
 	res := rec.Result()
 	if res.StatusCode != http.StatusOK {
 		t.Errorf("expected status Ok, but got %v", res.Status)
 	}
-	b, err := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Fatalf("could not read response %v", err)
 	}
-	if string(b) != "}]" {
-		t.Fatalf("expected 2, but got %v", b)
+	if string(body) != "}]" {
+		t.Fatalf("expected 2, but got %v", body)
 	}
 }

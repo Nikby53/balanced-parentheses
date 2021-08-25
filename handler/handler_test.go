180c947951467c55b2a17e55d5ccfbede9c6e6da
nikby53 <nikdiablo3@gmail.com>
2021-08-25 15:26:30 +0300

    added new tests for a calculation handler

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -27,11 +27,32 @@ func HandlerTest(status int, parameter string) func(t *testing.T) {
 	}
 }
 
+func HandlerCalculationTest(status int, parameter string) func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Helper()
+		req := httptest.NewRequest("GET", "localhost:8081/calculate"+parameter, nil)
+		rec := httptest.NewRecorder()
+		h := New(serviceMock{})
+		h.CalculationHandler(rec, req)
+		res := rec.Result()
+		if res.StatusCode != status {
+			t.Errorf("expected status %v, but got %v", status, res.Status)
+		}
+	}
+}
+
 func TestGenerationHandler(t *testing.T) {
 	t.Run("success", HandlerTest(200, "?n=2"))
 	t.Run("number query parameters is required", HandlerTest(400, ""))
 	t.Run("query parameter should be number", HandlerTest(400, "hello"))
 	t.Run("parameter should be greater than zero", HandlerTest(400, "?n=-2"))
+}
+
+func TestHandler_CalculationHandler(t *testing.T) {
+	t.Run("success", HandlerCalculationTest(200, "?n=2"))
+	t.Run("number query parameters is required", HandlerCalculationTest(400, ""))
+	t.Run("query parameter should be number", HandlerCalculationTest(400, "hello"))
+	t.Run("parameter should be greater than zero", HandlerCalculationTest(400, "?n=-2"))
 }
 
 func TestGenerationRequest_Validate(t *testing.T) {

310484de0be890f195e752930002ab9330a0fed0
nikby53 <nikdiablo3@gmail.com>
2021-09-02 14:03:50 +0300

    Change name of files and some code refactoring

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,8 +10,12 @@ import (
 	"github.com/Nikby53/balanced-parentheses/brackets"
 )
 
-const requestURL = "http://localhost:8081/generate?n="
+const (
+	requestURL       = "http://localhost:8081/generate?n="
+	numberOfRequests = 1000
+)
 
+// request makes a request to a URL
 func request(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -25,37 +29,39 @@ func request(url string) (string, error) {
 	return string(body), nil
 }
 
+// workerPool implements the required number of cycles and writes
+// the result of isBalanced function to the balanced channel.
 func workerPool(url string, cycles <-chan int, balanced chan<- bool) {
 	for range cycles {
 		result, err := request(url)
 		if err != nil {
 			log.Println(err)
+			balanced <- false
+			continue
 		}
 		balanced <- brackets.IsBalanced(result)
 	}
 }
 
 func main() {
-	result := make(chan bool, 1000)
+	result := make(chan bool, numberOfRequests)
 	for length := 2; length <= 8; length *= 2 {
 		url := requestURL + strconv.Itoa(length)
-
-		request := make(chan int, 20)
-
+		cycles := make(chan int, 20)
 		for i := 0; i < 20; i++ {
-			go workerPool(url, request, result)
+			go workerPool(url, cycles, result)
 		}
-		for i := 0; i < 1000; i++ {
-			request <- i
+		for i := 0; i < numberOfRequests; i++ {
+			cycles <- i
 		}
-		close(request)
+		close(cycles)
 		var balanced int
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < numberOfRequests; i++ {
 			if <-result {
 				balanced++
 			}
 		}
-		percentOfBalanced := float64(balanced) / 1000 * 100
-		fmt.Printf("lenght %v, percentage of balanced: %.1f%%\n", length, percentOfBalanced)
+		percentOfBalanced := float64(balanced) / numberOfRequests * 100
+		fmt.Printf("length %v, percentage of balanced: %.1f%%\n", length, percentOfBalanced)
 	}
 }
